90c1e18e167769d1113eb45dcabe08df9c415fbd
codeaarambh <mrarav1234@gmail.com>
2024-02-23 22:06:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true,
+    smoothMobile: true // Is line ko add kiya hai
+});
+
+function landing(){
+    let time=gsap.timeline()
+    time.to(".page1",{
+        y:"120vh",
+        scale:0.5,
+        duration:0
+    })
+    time.to(".page1",{
+        y:"30vh",
+        duration:1,
+        delay:1
+    })
+    time.to(".page1",{
+        y:"0vh",
+       rotate:360,
+       scale:1,
+       duration:0.8
+    })
+}
+
+landing();
